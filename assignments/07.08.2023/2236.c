ce2079829086ca2d4fa09932ea6fdcc89ebf292b
kash2104 <kparikh1104@gmail.com>
2023-08-07 14:45:26 +0530

    Lab 1

--- assignments/07.08.2023/2236.c
+++ assignments/07.08.2023/2236.c
@@ -6,32 +6,6 @@ public:
 };
 
 
-class Solution {
-public:
-    int smallestEvenMultiple(int n) {
-        if(n % 2 == 0){
-            return n;
-        }
-        return n * 2;
-    }
-};
 
 
-class Solution {
-public:
-    int sumOfMultiples(int n) {
-        int sum = 0;
-        for(int i = 1; i <= n; i++){
-            if(i % 3 == 0){
-                sum += i;
-            }
-            else if(i % 5 == 0){
-                sum += i;
-            }
-            else if(i % 7 == 0){
-                sum += i;
-            }
-        }
-        return sum;
-    }
-};
+
